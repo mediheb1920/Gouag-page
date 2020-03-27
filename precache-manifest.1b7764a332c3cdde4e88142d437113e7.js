9514c419274fb192f432825b315e1eef7d801451
mediheb1920 <ihebhichri11@gmail.com>
2020-03-27 14:38:13 +0100

    Updates

---
+++ precache-manifest.1b7764a332c3cdde4e88142d437113e7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408661d2b4006679cd25fb2e4062a6a4",
+    "url": "/mediheb1920/Gouag-page.git/index.html"
+  },
+  {
+    "revision": "1d7f76c2c84a30a7013e",
+    "url": "/mediheb1920/Gouag-page.git/static/css/2.e225b4da.chunk.css"
+  },
+  {
+    "revision": "711624b4ccf525078d57",
+    "url": "/mediheb1920/Gouag-page.git/static/css/main.9d9ef703.chunk.css"
+  },
+  {
+    "revision": "1d7f76c2c84a30a7013e",
+    "url": "/mediheb1920/Gouag-page.git/static/js/2.6581b631.chunk.js"
+  },
+  {
+    "revision": "43b37360df7b2cae30f06c8890b4d029",
+    "url": "/mediheb1920/Gouag-page.git/static/js/2.6581b631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711624b4ccf525078d57",
+    "url": "/mediheb1920/Gouag-page.git/static/js/main.638b7361.chunk.js"
+  },
+  {
+    "revision": "aa2f5f604922455e1fe8",
+    "url": "/mediheb1920/Gouag-page.git/static/js/runtime-main.1271ce4e.js"
+  },
+  {
+    "revision": "0e28b2cec5b95624fe8edd4482e3665e",
+    "url": "/mediheb1920/Gouag-page.git/static/media/Controller.0e28b2ce.svg"
+  },
+  {
+    "revision": "cd7a3f46c2e507fe39a8c572bdfae886",
+    "url": "/mediheb1920/Gouag-page.git/static/media/Credit-card.cd7a3f46.svg"
+  },
+  {
+    "revision": "5391c81b5df3af5c613a6c51544a349b",
+    "url": "/mediheb1920/Gouag-page.git/static/media/Delivery.5391c81b.svg"
+  },
+  {
+    "revision": "dac66804a654aa96492c2a791badedae",
+    "url": "/mediheb1920/Gouag-page.git/static/media/Gifts.dac66804.svg"
+  },
+  {
+    "revision": "5029064019a15db1efc8e7578a02ad49",
+    "url": "/mediheb1920/Gouag-page.git/static/media/Gouha.50290640.png"
+  },
+  {
+    "revision": "885347d88cad3c467f01d5253fa67f25",
+    "url": "/mediheb1920/Gouag-page.git/static/media/Quality.885347d8.svg"
+  },
+  {
+    "revision": "1219feab570f1c0b64cba3321b883856",
+    "url": "/mediheb1920/Gouag-page.git/static/media/Shield.1219feab.svg"
+  },
+  {
+    "revision": "c97391051b6b4cbe34d7a1bfe040d88d",
+    "url": "/mediheb1920/Gouag-page.git/static/media/Spotify_Logo.c9739105.png"
+  },
+  {
+    "revision": "6499a752ff5aea10abcf00ad2c70cfae",
+    "url": "/mediheb1920/Gouag-page.git/static/media/acceuil-image.6499a752.svg"
+  },
+  {
+    "revision": "d515ad021ad03e8353dcbd971e2f282e",
+    "url": "/mediheb1920/Gouag-page.git/static/media/bg-shape.d515ad02.svg"
+  },
+  {
+    "revision": "9de670db1c525f294cab6e8383f2258c",
+    "url": "/mediheb1920/Gouag-page.git/static/media/coins.9de670db.svg"
+  },
+  {
+    "revision": "098fddce629c3dc6f0d32bc3e428043e",
+    "url": "/mediheb1920/Gouag-page.git/static/media/google-play.098fddce.png"
+  },
+  {
+    "revision": "2ea85d7448475a744c1485c2eac3d3d1",
+    "url": "/mediheb1920/Gouag-page.git/static/media/netflix-logo-png-2562.2ea85d74.png"
+  },
+  {
+    "revision": "d5170e50f366c08dda23361b47fdf767",
+    "url": "/mediheb1920/Gouag-page.git/static/media/playstation-4.d5170e50.png"
+  },
+  {
+    "revision": "ed4110d01b4b83f3a56fbb2da0d370eb",
+    "url": "/mediheb1920/Gouag-page.git/static/media/shop.ed4110d0.svg"
+  },
+  {
+    "revision": "3305f736c1a7ece16e8f1e5e1c44cc4e",
+    "url": "/mediheb1920/Gouag-page.git/static/media/shopping-cart.3305f736.svg"
+  },
+  {
+    "revision": "1c5b1a40770823110f6177732f8f92d8",
+    "url": "/mediheb1920/Gouag-page.git/static/media/steam.1c5b1a40.png"
+  }
+]);
